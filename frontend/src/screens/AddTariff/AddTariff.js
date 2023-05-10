05657936f8f028adcb68cc1b24fa4f9127f3c706
Nargiza Sainazarova <nargizasaina@gmail.com>
2023-05-10 17:16:09 +0600

    add tariff form logic

--- frontend/src/screens/AddTariff/AddTariff.js
+++ frontend/src/screens/AddTariff/AddTariff.js
@@ -4,39 +4,79 @@ import Button from "../../components/UI/Button/Button";
 import './AddTariff.css';
 
 const AddTariff = () => {
-  const [tariff, setTariff] = useState({
-    name: '',
-    services: []
-  });
+  const [name, setName] = useState('');
 
-  const inputHandler = e => {
+  const [services, setServices] = useState(['']);
 
+  const addServiceInput = () => {
+    console.log(services);
+    if (services[services.length-1] !== '' ) {
+      setServices(prev => [
+        ...prev,
+        ''
+      ]);
+    }
+  };
+
+  const removeService = index => {
+    setServices([
+      ...services.slice(0, index),
+      ...services.slice(index + 1)
+    ]);
+  };
+
+  const serviceChangeHandler = (e, index) => {
+    setServices(prev => {
+      const arr = [...prev];
+      arr[index] = e.replace(/ /g, '');
+      return arr;
+    });
+  };
+
+  const submitFormHandler = async e => {
+    e.preventDefault();
+    const filledServices = services.filter(item => item !== "");
+    console.log({name, services: filledServices});
   };
 
   return (
     <div className="container">
       <div className="addTariff">
-        <form>
+        <h3 className="title">Добавление тарифа</h3>
+        <form onSubmit={submitFormHandler}>
           <div className="input-wrapper">
             <TextField
               type="text"
               name="name"
-              value={tariff.name}
-              onChange={inputHandler}
+              value={name}
+              onChange={e => setName(e.target.value)}
               label="Наименование"
               required={true}
             />
           </div>
-          <div className="input-wrapper">
-            <TextField
-              type="text"
-              name="services"
-              value={tariff.services}
-              onChange={inputHandler}
-              label="Услуги"
-              required={true}
-            />
-          </div>
+          {services.map((item, i) => (
+            <div className="input-wrapper" key={i}>
+              <label htmlFor="services">Услуга</label>
+              <textarea 
+                wrap="soft" 
+                name="services"
+                id="services"
+                value={item}
+                onChange={e => serviceChangeHandler(e.target.value, i)}
+                label="Услуга"
+                className='service-input'
+              />
+              <button 
+                type="button" 
+                className="tariff-small-btn remove-btn" 
+                disabled={services.length === 1}
+                onClick={() => removeService(i)}></button>
+              <button 
+                type="button" 
+                className="tariff-small-btn add-btn" 
+                onClick={addServiceInput}></button>
+            </div>
+          ))}
           <Button
             type="submit"
             title="Сохранить"
